7b2a95ab0eac219ccc533ae1b98abb8007c4b48e
Raheem Salifu Kasim <r.kasim@aol.com>
2023-04-12 21:46:00 +0000

    Task Update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,24 +4,18 @@ create a web cach
 """
 import redis
 import requests
+rc = redis.Redis()
+count = 0
 
-r = redis.Redis()
-CACHE_EXPIRATION_TIME = 10
 
 def get_page(url: str) -> str:
-    # Check if the page is already cached
-    cached_value = r.get(f"cached:{url}")
-    if cached_value is not None:
-        # Increment the access count and return the cached value
-        r.incr(f"count:{url}")
-        return cached_value.decode()
-
-    # If the page is not cached, fetch it and cache it
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
     resp = requests.get(url)
-    r.set(f"cached:{url}", resp.text)
-    r.expire(f"cached:{url}", CACHE_EXPIRATION_TIME)
-    r.set(f"count:{url}", 1)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
     return resp.text
 
+
 if __name__ == "__main__":
-    print(get_page('http://slowwly.robertomurray.co.uk'))
+    get_page('http://slowwly.robertomurray.co.uk')
