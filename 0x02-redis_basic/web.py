810a70834778730e3b4182fd0aec2d31f081f4aa
Raheem Salifu Kasim <r.kasim@aol.com>
2023-04-12 21:38:52 +0000

    Task Update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,23 +1,29 @@
 import redis
 import requests
+from functools import wraps
 
 r = redis.Redis()
 CACHE_EXPIRATION_TIME = 10
 
+def cache_page(func):
+    @wraps(func)
+    def wrapper(url):
+        # Check if the page is already cached
+        cached_value = r.get(f"cached:{url}")
+        if cached_value is not None:
+            # Increment the access count and return the cached value
+            r.incr(f"count:{url}")
+            return cached_value.decode()
+
+        # If the page is not cached, fetch it and cache it
+        resp = func(url)
+        r.set(f"cached:{url}", resp.content)
+        r.expire(f"cached:{url}", CACHE_EXPIRATION_TIME)
+        r.set(f"count:{url}", 1)
+        return resp.text
+    return wrapper
+
+@cache_page
 def get_page(url: str) -> str:
-    # Check if the page is already cached
-    cached_value = r.get(f"cached:{url}")
-    if cached_value is not None:
-        # Increment the access count and return the cached value
-        r.incr(f"count:{url}")
-        return cached_value.decode()
+    return requests.get(url)
 
-    # If the page is not cached, fetch it and cache it
-    resp = requests.get(url)
-    r.set(f"cached:{url}", resp.content)
-    r.expire(f"cached:{url}", CACHE_EXPIRATION_TIME)
-    r.set(f"count:{url}", 1)
-    return resp.text
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
